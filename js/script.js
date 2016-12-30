9b762c86622f07550501cf27edf5ec7e5980768c
pranjal8694 <pranjalcoder@gmail.com>
2016-12-30 14:41:06 +0530

    first email reply changes

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var $ = jQuery.noConflict();
 
 // Page Loader
 $(window).load(function () {
-    
+
     "use strict";
 	$('#loader').fadeOut();
 });
@@ -12,74 +12,74 @@ $(window).load(function () {
 
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function () {
-    
-    "use strict";
-    
-    $('a.page-scroll').bind('click', function (event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 68
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
+// $(function () {
+//
+//     "use strict";
+//
+//     $('a.page-scroll').bind('click', function (event) {
+//         var $anchor = $(this);
+//         $('html, body').stop().animate({
+//             scrollTop: $($anchor.attr('href')).offset().top - 68
+//         }, 1500, 'easeInOutExpo');
+//         event.preventDefault();
+//     });
+// });
 
 
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top',
-	offset: 70
-	
+	offset: 60
+
 });
 
 
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function () {
-    "use strict";
-    $('.navbar-toggle:visible').click();
-});
-    
-    
-    
-    
+// $('.navbar-collapse ul li a').click(function () {
+//     "use strict";
+//     $('.navbar-toggle:visible').click();
+// });
+
+
+
+
     /*----------------------------------------------------*/
     /*	Css3 Transition
     /*----------------------------------------------------*/
 
-        $('*').each(function(){
-            if($(this).attr('data-animation')) {
-                var $animationName = $(this).attr('data-animation'),
-                    $animationDelay = "delay-"+$(this).attr('data-animation-delay');
-                $(this).appear (function() {
-                    $(this).addClass('animated').addClass($animationName);
-                    $(this).addClass('animated').addClass($animationDelay);
-                });
-            }
-        });
-    
-    
+        // $('*').each(function(){
+        //     if($(this).attr('data-animation')) {
+        //         var $animationName = $(this).attr('data-animation'),
+        //             $animationDelay = "delay-"+$(this).attr('data-animation-delay');
+        //         $(this).appear (function() {
+        //             $(this).addClass('animated').addClass($animationName);
+        //             $(this).addClass('animated').addClass($animationDelay);
+        //         });
+        //     }
+        // });
 
-    
+
+
+
     /*--------------------------------------------------*/
     /* Counter*/
-    /*--------------------------------------------------*/ 
-        
+    /*--------------------------------------------------*/
 
-                $('.timer').countTo();
 
-                $('.counter-item').appear(function() {
-                    $('.timer').countTo();
-                },{accY: -100});
-            
-        
+                  $('.timer').countTo();
+                  //
+                  // $('.counter-item').appear(function() {
+                  //     $('.timer').countTo();
+                  // },{accY: -100});
+
+
 
 
 
 ////------- Testimonials Carousel
-	
+
 var testimonial = $("#testimonial-carousel");
 
 testimonial.owlCarousel({
@@ -87,75 +87,34 @@ testimonial.owlCarousel({
 	pagination: true,
 	slideSpeed : 2500,
 	stopOnHover: true,
-	autoPlay: 3000,
+	autoPlay: 2000,
 	singleItem: true,
 	transitionStyle : "fade",
 	navigationText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>']
 });
 
-	
+
 
 ////------- Pricing Table
-
-$(document).ready(function() {
- 
-  $(".pricing").owlCarousel({
-        pagination: true,
-        navigation : false,
-        slideSpeed : 2500,
-		stopOnHover: true,
-    	autoPlay: 3000,
-    	singleItem:false,
-        itemsMobile : [550,1],
-        itemsDesktopSmall : [991,2],
-        items: 4,
-		transitionStyle : "fade",
-		navigationText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>']
-  });
- 
-});
-
-
-////------- Latest News
-
-$(document).ready(function() {
- 
-  $(".latest-news").owlCarousel({
-        pagination: true,
-        navigation : false,
-        slideSpeed : 2500,
-		stopOnHover: true,
-    	autoPlay: 4000,
-    	singleItem:false,
-        itemsMobile : [550,1],
-        itemsDesktopSmall : [991,2],
-        items: 3,
-		transitionStyle : "fade",
-		navigationText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>']
-  });
- 
-});
-
-
 
 
 // Team Carousel
 
 $(document).ready(function() {
- 
+
   $(".our-team").owlCarousel({
-        pagination: true,
-        slideSpeed : 2500,
-		stopOnHover: true,
-    	autoPlay: 3000,
+        // pagination: true,
+        // slideSpeed : 2500,
+		// stopOnHover: true,
+    	// autoPlay: 3000,
         items: 4,
     	//singleItem:true,
-        itemsMobile : [550,1],
-        itemsDesktopSmall : [991,2],
-		transitionStyle : "fade",
-		navigationText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>']
+        // itemsMobile : [550,1],
+        // itemsDesktopSmall : [991,2],
+		// transitionStyle : "fade",
+		// navigationText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>']
   });
- 
+
 });
 
 
@@ -163,7 +122,7 @@ $(document).ready(function() {
 ////------- Partner/CLients
 
 $(document).ready(function() {
- 
+
   $(".clients").owlCarousel({
         pagination: false,
         navigation : true,
@@ -177,29 +136,29 @@ $(document).ready(function() {
 		transitionStyle : "fade",
 		navigationText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>']
   });
- 
+
 });
 
 
 
 
-$ ( function () {
-
-	$(".video").fitVids();
-	
-});
-
-
-
-
-
-        // Tab------------------------------
-
-
-$('#team a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-})
+// $ ( function () {
+//
+// 	$(".video").fitVids();
+//
+// });
+//
+//
+//
+//
+//
+//         // Tab------------------------------
+//
+//
+// $('#team a').click(function (e) {
+//   e.preventDefault()
+//   $(this).tab('show')
+// })
 
 
 
@@ -207,16 +166,14 @@ $('#team a').click(function (e) {
 /*---------------------------------------------------*/
     /* Progress Bar
     /*---------------------------------------------------*/
-    $(document).ready(function($) {
-	"use strict";
-    
-        $('.skill-shortcode').appear(function() {
-            $('.progress').each(function(){ 
-                $('.progress-bar').css('width',  function(){ return ($(this).attr('data-percentage')+'%')});
-            });
-        },{accY: -100});
-        
-        
-    });
-	
-
+  //   $(document).ready(function($) {
+	// "use strict";
+  //
+  //       $('.skill-shortcode').appear(function() {
+  //           $('.progress').each(function(){
+  //               $('.progress-bar').css('width',  function(){ return ($(this).attr('data-percentage')+'%')});
+  //           });
+  //       },{accY: -100});
+  //
+  //
+  //   });
